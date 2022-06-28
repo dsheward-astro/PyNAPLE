0fd8183ad004cbfc5c72a6ece61a6a56782e80c2
dsheward-astro <djs22@aber.ac.uk>
2022-06-28 11:00:24 +0100

    added comment

--- runIsis.sh
+++ runIsis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set ISISROOT here if not set in your environment and delete #
 
-ISISROOT=/usr/local/Isis/isis
+#ISISROOT=/path/to/Isis/isis
+
 . /$ISISROOT/scripts/isis3Startup.sh isis3
